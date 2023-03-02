88c58c25eb559bf5ef2dc880d28f5d82caae4328
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-03-02 15:46:05 -0300

    Finished challenge 08.

--- src/pages/challenge-08/components/CardSalesByDayOfTheWeek/index.tsx
+++ src/pages/challenge-08/components/CardSalesByDayOfTheWeek/index.tsx
@@ -1,22 +1,66 @@
+import dashboardIconTriangleDownImg from "@/assets/images/dashboard-icon-triangle-down.png";
+import dashboardIconTriangleUpImg from "@/assets/images/dashboard-icon-triangle-up.png";
+import Image from "next/image";
 import { DefaultCard } from "../DefaultCard";
-import { CardGroup } from "./styles";
+import { CardGroup, CardInfo, Graphic, GraphicBar, MoreSales } from "./styles";
 
 export function CardSalesByDayOfTheWeek() {
   return (
     <DefaultCard>
       <h2>Vendas por dia da semana</h2>
       <CardGroup>
-        <div>
-          <div>
-            <small>Dias com mais vendas</small>
+        <CardInfo>
+          <MoreSales>
+            <small>
+              <Image
+                src={dashboardIconTriangleUpImg}
+                alt="Ícone de triangulo verde apontado para cima."
+              />
+              Dias com mais vendas
+            </small>
             <span>quarta-feira</span>
-          </div>
-          <div>
-            <small>Dias com menos vendas</small>
+          </MoreSales>
+          <MoreSales>
+            <small>
+              <Image
+                src={dashboardIconTriangleDownImg}
+                alt="Ícone de triangulo verde apontado para baixo."
+              />
+              Dias com menos vendas
+            </small>
             <span>domingo</span>
-          </div>
-        </div>
-        <div>Gráfico</div>
+          </MoreSales>
+        </CardInfo>
+        <Graphic>
+          <GraphicBar css={{ "--height": "2.4375rem" }}>
+            <div />
+            <span>dom</span>
+          </GraphicBar>
+          <GraphicBar css={{ "--height": "7.1875rem" }}>
+            <div />
+            <span>seg</span>
+          </GraphicBar>
+          <GraphicBar css={{ "--height": "4.75rem" }}>
+            <div />
+            <span>ter</span>
+          </GraphicBar>
+          <GraphicBar css={{ "--height": "9.9375rem" }}>
+            <div />
+            <span>qua</span>
+          </GraphicBar>
+          <GraphicBar css={{ "--height": "8.0625rem" }}>
+            <div />
+            <span>qui</span>
+          </GraphicBar>
+          <GraphicBar css={{ "--height": "7.5rem" }}>
+            <div />
+            <span>sex</span>
+          </GraphicBar>
+          <GraphicBar css={{ "--height": "4.3125rem" }}>
+            <div />
+            <span>sab</span>
+          </GraphicBar>
+        </Graphic>
       </CardGroup>
     </DefaultCard>
   );
--- src/pages/challenge-08/components/CardSalesByDayOfTheWeek/styles.ts
+++ src/pages/challenge-08/components/CardSalesByDayOfTheWeek/styles.ts
@@ -6,3 +6,71 @@ export const CardGroup = styled("div", {
   justifyContent: "space-between",
   gap: "2rem",
 });
+
+export const CardInfo = styled("div", {
+  flex: 1,
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fit, minmax(14.6rem, auto))",
+  gap: "2rem",
+});
+
+export const MoreSales = styled("div", {
+  display: "grid",
+  gap: ".5rem",
+  fontWeight: 500,
+  lineHeight: "160%",
+  whiteSpace: "nowrap",
+
+  small: {
+    display: "flex",
+    alignItems: "center",
+    gap: ".25rem",
+    fontSize: ".875rem",
+  },
+
+  span: {
+    fontSize: "1.5rem",
+  },
+});
+
+export const Graphic = styled("div", {
+  flex: 1,
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "flex-end",
+  gap: "1.5rem",
+  position: "relative",
+
+  "&::before": {
+    position: "absolute",
+    content: "",
+    display: "block",
+    width: "100%",
+    height: ".1875rem",
+    background: "#4a4556",
+    borderRadius: "999px",
+    top: "50%",
+    transform: "translateY(-50%)",
+    zIndex: 0,
+  },
+});
+
+export const GraphicBar = styled("div", {
+  display: "grid",
+  justifyItems: "center",
+  gap: ".3125rem",
+  zIndex: 1,
+
+  div: {
+    width: ".9375rem",
+    height: "var(--height)",
+    background: "linear-gradient(180deg, #90f7ec 0%, #32ccbc 100%)",
+    borderRadius: "999px",
+  },
+
+  span: {
+    fontWeight: 500,
+    fontSize: ".875rem",
+    lineHeight: "160%",
+  },
+});
